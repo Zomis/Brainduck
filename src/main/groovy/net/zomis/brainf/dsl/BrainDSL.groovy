1ca3af0ef2478fc595270b2a54982d1daf1b42c1
Simon Forsberg <zomis2k@hotmail.com>
2015-07-04 01:49:27 +0200

    added some set methods and a test

--- src/main/groovy/net/zomis/brainf/dsl/BrainDSL.groovy
+++ src/main/groovy/net/zomis/brainf/dsl/BrainDSL.groovy
@@ -30,6 +30,56 @@ class BrainDSL {
         addCode('>' * steps)
     }
 
+    BrainDSL set(Object... args) {
+        for (int i = 0; i < args.length; i++) {
+            if (i > 0) {
+                addCode('>')
+            }
+            println 'set ' + args[i]
+            set(args[i])
+        }
+        this
+    }
+
+    BrainDSL set(Object text) {
+        if (text == null) {
+            throw new IllegalArgumentException('text cannot be null')
+        }
+        println 'set ' + text + ' of type ' + text.class
+        if (text instanceof Integer) {
+            set(text as int)
+        } else if (text instanceof String) {
+            set text as String
+        } else if (text instanceof Character) {
+            set String.valueOf(text)
+        } else {
+            throw new IllegalArgumentException('Invalid type: ' + text)
+        }
+        this
+    }
+
+    BrainDSL set(String text) {
+        println 'set text ' + text + ' length ' + text.length()
+        for (int i = 0; i < text.length(); i++) {
+            if (i > 0) {
+                addCode('>')
+            }
+            char ch = text.charAt(i)
+            set(ch as int)
+        }
+        this
+    }
+
+    BrainDSL set(int value) {
+        println 'set int ' + value
+        addCode('[-]')
+        if (value > 0) {
+            addCode('+' * value)
+        } else {
+            addCode('-' * -value)
+        }
+    }
+
     BrainDSL addCode(String s) {
         code.append(s)
         this
