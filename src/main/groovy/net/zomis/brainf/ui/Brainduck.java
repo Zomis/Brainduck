30c8d0f714f09724d4a5913f24ba2dbded39a2f2
Simon Forsberg <zomis2k@hotmail.com>
2015-09-11 01:45:13 +0200

    Change name of main class

--- src/main/groovy/net/zomis/brainf/ui/Brainduck.java
+++ src/main/groovy/net/zomis/brainf/ui/Brainduck.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class BrainFDebug extends Application {
+public class Brainduck extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
