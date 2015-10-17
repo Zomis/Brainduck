70142c3d88f0e0e1ebf4d002f674279265046621
Simon Forsberg <zomis2k@hotmail.com>
2015-10-17 15:19:49 +0200

    Rework how `StepContinueStrategy` works, fixes #25

--- src/main/groovy/net/zomis/brainf/model/run/StepContinueStrategy.groovy
+++ src/main/groovy/net/zomis/brainf/model/run/StepContinueStrategy.groovy
@@ -6,33 +6,56 @@ import net.zomis.brainf.model.BrainfuckRunner
 
 class StepContinueStrategy implements RunStrategy {
 
-    private int loops
+    private int loopStart
+    private int loopEnd
+    private int nextLoop
+    private boolean performedOnce
+    private final RunStrategy singleStep = new SingleStepStrategy()
 
     @Override
     boolean start(BrainfuckRunner runner) {
-        loops = 0
-        if (runner.getCode().getNextCommand() == BrainFCommand.WHILE) {
-            return false
-        }
-        int matching = runner.getCode().findMatching(BrainFCommand.END_WHILE, BrainFCommand.WHILE, 1)
-        return matching != -1
+        loopStart = runner.code.findMatching(BrainFCommand.WHILE,
+            BrainFCommand.END_WHILE, -1)
+        loopEnd = runner.code.findMatching(loopStart, BrainFCommand.END_WHILE,
+                BrainFCommand.WHILE, 1)
+        nextLoop = runner.code.findMatching(BrainFCommand.WHILE,
+                BrainFCommand.END_WHILE, 1)
+        performedOnce = false
+        println 'loopStart at ' + loopStart + ' and end at ' + loopEnd + ' next loop at ' + nextLoop
+
+        return singleStep.start(runner)
     }
 
     @Override
     boolean next(BrainfuckRunner runner) {
         BrainfuckCommand comm = runner.getCode().getNextCommand()
-        if (comm == BrainFCommand.WHILE) {
-            loops++
-        }
-        if (comm == BrainFCommand.END_WHILE && runner.getMemory().getValue() == 0) {
-            loops--
-        }
-        if (comm == BrainFCommand.END_WHILE && loops <= 0) {
-            runner.step()
+        if (comm == null) {
             return false
         }
-        runner.step();
-        return true
+
+        int codeIndex = runner.code.commandIndex
+        if (loopStart != -1 && loopEnd != -1) {
+            // if inside loop, find the correct `END_WHILE` inside this loop and go to it and one step beyond
+            boolean perform = codeIndex != loopStart + 1 && codeIndex != loopEnd + 1
+            if (!performedOnce) {
+                perform = true
+                performedOnce = true
+            }
+            if (perform) {
+                runner.step()
+            }
+            return perform
+        } else if (nextLoop != -1) {
+            // Not inside loop, go to next starting loop if there is one
+            if (codeIndex != nextLoop + 1) {
+                runner.step()
+                return true
+            }
+            return false
+        } else {
+            // if not inside loop and no next loop, do the same as `StepNext`
+            return singleStep.next(runner)
+        }
     }
 
 }
