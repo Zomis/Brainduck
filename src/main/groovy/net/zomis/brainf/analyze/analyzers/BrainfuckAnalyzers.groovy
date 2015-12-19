4bb51b99765e1b91ae7913b79e2cc840b94d6666
Simon Forsberg <zomis2k@hotmail.com>
2015-12-19 23:46:46 +0100

    Add `CodeCellRelationAnalysis`, addressing #17

--- src/main/groovy/net/zomis/brainf/analyze/analyzers/BrainfuckAnalyzers.groovy
+++ src/main/groovy/net/zomis/brainf/analyze/analyzers/BrainfuckAnalyzers.groovy
@@ -14,6 +14,7 @@ class BrainfuckAnalyzers {
                 new CommandCountAnalysis(),
                 new MemoryIndexAnalysis(),
                 new PlusMinusOptimizer(),
+                new CodeCellRelationAnalysis(),
         ]
         analyzers
     }
---
+++ src/main/groovy/net/zomis/brainf/analyze/analyzers/CodeCellRelationAnalysis.groovy
@@ -0,0 +1,58 @@
+package net.zomis.brainf.analyze.analyzers
+
+import net.zomis.brainf.analyze.Brainalyze
+import net.zomis.brainf.analyze.BrainfuckAnalyzer
+import net.zomis.brainf.analyze.MemoryCell
+import net.zomis.brainf.model.BrainfuckCommand
+import net.zomis.brainf.model.BrainfuckRunner
+
+class CodeCellRelationAnalysis implements BrainfuckAnalyzer {
+
+    Map<Integer, Set<Integer>> codeToCells = new HashMap<>()
+    Map<Integer, Set<Integer>> cellsToCode = new HashMap<>()
+
+    static void add(Map<Integer, Set<Integer>> map, int key, int value) {
+        map.putIfAbsent(key, new HashSet<Integer>())
+        map.get(key).add(value)
+    }
+
+    @Override
+    Object createMemoryData() {
+        return null // new CodeCellRelationship()
+    }
+
+    @Override
+    void beforeStart(BrainfuckRunner runner) {
+    }
+
+    @Override
+    void after(Brainalyze analyze, BrainfuckRunner runner) {
+    }
+
+    @Override
+    void print() {
+        println 'Code to cells'
+        codeToCells.forEach({key, value ->
+            println "$key --> $value"
+        })
+        println ''
+        println 'Cells to code'
+        cellsToCode.forEach({key, value ->
+            println "$key --> $value"
+        })
+        println ''
+    }
+
+    @Override
+    void beforePerform(MemoryCell cell, BrainfuckRunner runner, BrainfuckCommand command) {
+//        def data = cell.data(this, CodeCellRelationship)
+        int codeIndex = runner.code.commandIndex
+        int cellIndex = runner.memory.memoryIndex
+        add(cellsToCode, cellIndex, codeIndex)
+        add(codeToCells, codeIndex, cellIndex)
+    }
+
+    @Override
+    void afterPerform(MemoryCell cell, BrainfuckRunner runner, BrainfuckCommand command) {
+    }
+}
