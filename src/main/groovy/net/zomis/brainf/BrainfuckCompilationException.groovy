b5964db6153a1608bbb99258ccb738afa0f03fcd
Simon Forsberg <zomis2k@hotmail.com>
2016-12-06 23:11:39 +0100

    Make input great again

---
+++ src/main/groovy/net/zomis/brainf/BrainfuckCompilationException.groovy
@@ -0,0 +1,7 @@
+package net.zomis.brainf
+
+/**
+ * Created by Simon on 12/6/2016.
+ */
+class BrainfuckCompilationException {
+}
--- src/main/groovy/net/zomis/brainf/model/ast/tree/ReadSyntax.java
+++ src/main/groovy/net/zomis/brainf/model/ast/tree/ReadSyntax.java
@@ -1,13 +1,19 @@
 package net.zomis.brainf.model.ast.tree;
 
+import net.zomis.brainf.model.BrainfuckException;
 import net.zomis.brainf.model.BrainfuckRunner;
 
 public class ReadSyntax extends Syntax {
 
-
     @Override
     public void perform(BrainfuckRunner runner) {
-
+        int value;
+        try {
+            value = runner.getInput().read();
+        } catch (BrainfuckException e) {
+            throw new RuntimeException(e);
+        }
+        runner.getMemory().setMemory(value);
     }
 
 }
