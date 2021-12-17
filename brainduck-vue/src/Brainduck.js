89e9b1870e60dbd05f2ac5e42357b11435ac7898
Simon Forsberg <zomis2k@hotmail.com>
2021-12-18 00:11:19 +0100

    Add new Vue project

---
+++ brainduck-vue/src/Brainduck.js
@@ -0,0 +1,19 @@
+(function (_, Kotlin) {
+  'use strict';
+  var Kind_CLASS = Kotlin.Kind.CLASS;
+  function Program() {
+  }
+  Program.$metadata$ = {
+    kind: Kind_CLASS,
+    simpleName: 'Program',
+    interfaces: []
+  };
+  var package$net = _.net || (_.net = {});
+  var package$zomis = package$net.zomis || (package$net.zomis = {});
+  var package$brainduck = package$zomis.brainduck || (package$zomis.brainduck = {});
+  package$brainduck.Program = Program;
+  Kotlin.defineModule('Brainduck', _);
+  return _;
+}(module.exports, require('kotlin')));
+
+//# sourceMappingURL=Brainduck.js.map
